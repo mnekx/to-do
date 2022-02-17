104d3dbc4c80f397c370e400018b6f46f4edf2f9
Mnemba Chambuya <karimnembac@gmail.com>
2022-02-17 13:37:43 +0300

    fix linter errors

--- src/task_collection.js
+++ src/task_collection.js
@@ -1,5 +1,4 @@
-import { times } from "lodash";
-import { initializeLocalStorage, populateStorage } from "./local_storage.js";
+import { initializeLocalStorage, populateStorage } from './local_storage.js';
 
 export default class TaskCollection {
   static counter = 0;
@@ -7,10 +6,9 @@ export default class TaskCollection {
   constructor() {
     this.collection = [];
     TaskCollection.counter = this.collection.length;
-    const tasksArr =
-      initializeLocalStorage(this.collection) !== null
-        ? initializeLocalStorage(this.collection)
-        : [];
+    const tasksArr = initializeLocalStorage(this.collection) !== null
+      ? initializeLocalStorage(this.collection)
+      : [];
     this.setTasks(tasksArr);
   }
 
@@ -22,7 +20,7 @@ export default class TaskCollection {
     populateStorage(this.getCollection());
 
     // Append task to the markup
-    const ul = document.querySelector("ul");
+    const ul = document.querySelector('ul');
     ul.appendChild(this.returnTaskLIMarkup(newTask));
 
     // Add event listeners
@@ -34,66 +32,65 @@ export default class TaskCollection {
 
   addTaskRemoveEventListener(index) {
     const removeBtn = document.querySelector(`#remove-btn-${index}`);
-    removeBtn.addEventListener("click", () => {
+    removeBtn.addEventListener('click', () => {
       this.remove(index);
     });
   }
 
   addTaskEditBtnEventListener(index) {
     const editBtn = document.querySelector(`#edit-btn-${index}`);
-    editBtn.addEventListener("click", () => {
+    editBtn.addEventListener('click', () => {
       const controls = document.querySelector(`#controls-${index}`);
-      controls.classList.add("d-none");
+      controls.classList.add('d-none');
       const label = document.querySelector(`#label-${index}`);
-      label.classList.add("d-none");
+      label.classList.add('d-none');
       const editInput = document.querySelector(`#edit-input-${index}`);
-      editInput.classList.remove("d-none");
+      editInput.classList.remove('d-none');
     });
     this.getCollection();
   }
 
   addTaskEditInputEventListener(index) {
     const editInput = document.querySelector(`#edit-input-${index}`);
-    editInput.addEventListener("keypress", (e) => {
+    editInput.addEventListener('keypress', (e) => {
       if (e.keyCode === 13) {
         this.editTask(index, e.target.value);
         e.preventDefault();
 
         const controls = document.querySelector(`#controls-${index}`);
-        controls.classList.remove("d-none");
+        controls.classList.remove('d-none');
         const label = document.querySelector(`#label-${index}`);
-        label.innerHTML = "";
-        label.classList.remove("d-none");
-        const status = document.createElement("input");
-        status.setAttribute("type", "checkbox");
+        label.innerHTML = '';
+        label.classList.remove('d-none');
+        const status = document.createElement('input');
+        status.setAttribute('type', 'checkbox');
         status.id = `task${index}`;
-        status.setAttribute("name", `status-${index}`);
+        status.setAttribute('name', `status-${index}`);
         label.appendChild(status);
         label.append(e.target.value);
         const editInput = document.querySelector(`#edit-input-${index}`);
-        editInput.classList.add("d-none");
+        editInput.classList.add('d-none');
       }
     });
   }
 
   addStatusEventListener(index) {
-    let statusInput = document.querySelector("#task" + index);
-    statusInput.addEventListener("change", (e) => {
+    const statusInput = document.querySelector(`#task${index}`);
+    statusInput.addEventListener('change', () => {
       this.toggleStatus(index);
       populateStorage(this.getCollection());
-      const containingLI = document.querySelector("#task-" + index);
-      containingLI.classList.toggle("completed");
+      const containingLI = document.querySelector(`#task-${index}`);
+      containingLI.classList.toggle('completed');
     });
   }
 
   toggleStatus(index) {
-    this.collection[index - 1]["completed"] =
-      !this.collection[index - 1]["completed"];
+    this.collection[index - 1].completed = !this.collection[index - 1].completed;
   }
 
   remove(idx) {
     this.collection = this.collection.filter(
-      (task) => task.index !== parseInt(idx, 10)
+      (task) => task.index !== parseInt(idx, 10),
     );
 
     const toRemove = document.querySelector(`#task-${idx}`);
@@ -119,8 +116,8 @@ export default class TaskCollection {
   }
 
   render() {
-    const ul = document.querySelector("ul");
-    ul.innerHTML = "";
+    const ul = document.querySelector('ul');
+    ul.innerHTML = '';
     this.resetIndexes();
     this.getCollection().forEach((task) => {
       ul.appendChild(this.returnTaskLIMarkup(task));
@@ -134,41 +131,42 @@ export default class TaskCollection {
   }
 
   returnTaskLIMarkup(task) {
-    const listItem = document.createElement("li");
-    listItem.className = "d-flex item";
+    const listItem = document.createElement('li');
+    listItem.className = 'd-flex item';
     listItem.id = `task-${task.index}`;
-    listItem.setAttribute("draggable", true);
-    const label = document.createElement("label");
+    listItem.setAttribute('draggable', true);
+    const label = document.createElement('label');
     label.id = `label-${task.index}`;
-    label.setAttribute("for", `task${task.index}`);
-    const status = document.createElement("input");
-    status.setAttribute("type", "checkbox");
+    label.setAttribute('for', `task${task.index}`);
+    const status = document.createElement('input');
+    status.setAttribute('type', 'checkbox');
     status.id = `task${task.index}`;
-    status.setAttribute("name", `status-${task.index}`);
-    status.className = "status-input";
+    status.setAttribute('name', `status-${task.index}`);
+    status.className = 'status-input';
     label.appendChild(status);
     label.append(task.description);
     listItem.appendChild(label);
-    const controls = document.createElement("div");
+    const controls = document.createElement('div');
     controls.id = `controls-${task.index}`;
-    controls.classList.add("controls");
-    const removeBtn = document.createElement("button");
-    removeBtn.classList.add("remove-btn");
+    controls.classList.add('controls');
+    const removeBtn = document.createElement('button');
+    removeBtn.classList.add('remove-btn');
     removeBtn.id = `remove-btn-${task.index}`;
-    removeBtn.innerText = "Delete";
+    removeBtn.innerText = 'Delete';
     controls.appendChild(removeBtn);
-    const editBtn = document.createElement("button");
-    editBtn.classList.add("edit-btn");
+    const editBtn = document.createElement('button');
+    editBtn.classList.add('edit-btn');
     editBtn.id = `edit-btn-${task.index}`;
-    editBtn.innerText = "Edit";
+    editBtn.innerText = 'Edit';
     controls.appendChild(editBtn);
-    const editInput = document.createElement("input");
-    editInput.setAttribute("type", "text");
-    editInput.className = "edit-input d-none";
+    const editInput = document.createElement('input');
+    editInput.setAttribute('type', 'text');
+    editInput.className = 'edit-input d-none';
     editInput.value = task.description;
     editInput.id = `edit-input-${task.index}`;
     listItem.appendChild(editInput);
     listItem.appendChild(controls);
+    this.getCollection();
 
     return listItem;
   }
@@ -179,7 +177,7 @@ export default class TaskCollection {
 
   editTask(index, data) {
     const idx = this.collection.findIndex(
-      (task) => task.index === parseInt(index, 10)
+      (task) => task.index === parseInt(index, 10),
     );
     this.collection[idx].description = data;
     populateStorage(this.getCollection());
