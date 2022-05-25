3fcd46ab69dd781520c68021895674a46839f456
mnekx <karimnembac@gmail.com>
2022-05-25 13:41:17 +0300

    Update task_collection.js

--- src/task_collection.js
+++ src/task_collection.js
@@ -1,6 +1,6 @@
 import { initializeLocalStorage, populateStorage } from './local_storage.js';
 import '@fortawesome/fontawesome-free/js/all.js';
-import resetStylesFormerEditedTask from './helpers.js';
+import { resetStylesFormerEditedTask, returnTaskLIMarkup, addEditTaskEventListener } from './helpers.js';
 
 export default class TaskCollection {
   static counter = 0;
@@ -19,15 +19,18 @@ export default class TaskCollection {
     const newTask = { index: idx, description: desc, completed: false };
     this.collection.push(newTask);
 
+    const badge = document.querySelector('legend div div');
+    badge.innerHTML = this.getUncompletedTasksNo();
+
     populateStorage(this.getCollection());
 
     // Append task to the markup
     const ul = document.querySelector('ul');
-    ul.appendChild(this.returnTaskLIMarkup(newTask));
+    ul.appendChild(returnTaskLIMarkup(newTask));
 
     // Add event listeners
     this.addTaskRemoveEventListener(newTask.index);
-    this.addEditTaskEventListener(newTask.index);
+    addEditTaskEventListener(newTask.index);
     this.addTaskEditInputEventListener(newTask.index);
     this.addStatusEventListener(newTask.index);
     // addBlurTaskEventListener(newTask.index);
@@ -39,45 +42,16 @@ export default class TaskCollection {
       trashBtn.addEventListener('click', (e) => {
         e.stopPropagation();
         this.remove(index);
-      });
+      }, { capture: true });
     }
-  }
-
-  addEditTaskEventListener(index) {
-    const task = document.querySelector(`#task-${index}`);
-    task.addEventListener('click', () => {
-      const formerEditedTask = document.querySelector('.edit-task');
-      if (formerEditedTask) {
-        const formerEditedTaskID = formerEditedTask.id.split('-').reverse()[0];
-        resetStylesFormerEditedTask(formerEditedTaskID);
-      }
-
-      const label = document.querySelector(`#label-${index}`);
-      const taskText = label.innerHTML;
-      const ellipsisBtn = document.querySelector(`#ellispis-btn-${index}`);
-      ellipsisBtn.classList.add('d-none');
-      const trashBtn = document.querySelector(`#trash-btn-${index}`);
-      trashBtn.classList.remove('d-none');
-      label.classList.add('d-none');
-      const editInput = document.querySelector(`#edit-input-${index}`);
-      editInput.classList.remove('d-none');
-      editInput.classList.add('d-block');
-      editInput.value = taskText;
-      editInput.focus();
-      task.classList.add('edit-task');
-    }, { capture: true });
-    this.getCollection();
   }
 
   addTaskEditInputEventListener(index) {
     const editInput = document.querySelector(`#edit-input-${index}`);
-    editInput.addEventListener(
-      'focusout',
-      (e) => {
-        // e.preventDefault();
-        e.stopPropagation();
-      },
-    );
+    editInput.addEventListener('focusout', (e) => {
+      // e.preventDefault();
+      e.stopPropagation();
+    });
     editInput.addEventListener('keypress', (e) => {
       if (e.keyCode === 13) {
         this.editTask(index, e.target.value);
@@ -92,17 +66,16 @@ export default class TaskCollection {
 
   addStatusEventListener(index) {
     const statusInput = document.querySelector(`#task${index}`);
-    statusInput.addEventListener(
-      'click',
-      (e) => {
-        e.stopPropagation();
-        resetStylesFormerEditedTask(index);
-        this.toggleStatus(index);
-        populateStorage(this.getCollection());
-        const containingLabel = document.querySelector(`#label-${index}`);
-        containingLabel.classList.toggle('completed');
-      },
-    );
+    statusInput.addEventListener('click', (e) => {
+      e.stopPropagation();
+      resetStylesFormerEditedTask(index);
+      this.toggleStatus(index);
+      const badge = document.querySelector('legend div div');
+      badge.innerHTML = this.getUncompletedTasksNo();
+      populateStorage(this.getCollection());
+      const containingLabel = document.querySelector(`#label-${index}`);
+      containingLabel.classList.toggle('completed');
+    }, { capture: true });
   }
 
   toggleStatus(index) {
@@ -122,6 +95,11 @@ export default class TaskCollection {
     this.render();
   }
 
+  updateTaskIndex(index, newTaskIndex) {
+    this.collection[index].index = newTaskIndex;
+    populateStorage(this.collection);
+  }
+
   resetIndexes() {
     for (let idx = 1; idx <= this.getCollection().length; idx += 1) {
       const task = this.getCollection()[idx - 1];
@@ -129,8 +107,19 @@ export default class TaskCollection {
     }
   }
 
+  swapTaskPositions(index1, index2) {
+    const temp = this.collection[index1];
+    this.collection[index1] = this.collection[index2];
+    this.collection[index2] = temp;
+  }
+
   getCollection() {
     return this.collection;
+  }
+
+  orderIndexwise() {
+    this.collection = this.collection.sort((a, b) => a.index - b.index);
+    populateStorage(this.collection);
   }
 
   setTasks(tasksArr) {
@@ -139,73 +128,20 @@ export default class TaskCollection {
 
   render() {
     const ul = document.querySelector('ul');
+    const badge = document.querySelector('legend div div');
+    badge.innerHTML = this.getUncompletedTasksNo();
     ul.innerHTML = '';
     this.resetIndexes();
     this.getCollection().forEach((task) => {
-      ul.appendChild(this.returnTaskLIMarkup(task));
+      ul.appendChild(returnTaskLIMarkup(task));
 
       //   Add event listeners
       this.addTaskRemoveEventListener(task.index);
-      this.addEditTaskEventListener(task.index);
+      addEditTaskEventListener(task.index);
       this.addTaskEditInputEventListener(task.index);
       this.addStatusEventListener(task.index);
       // addBlurTaskEventListener(task.index);
     });
-  }
-
-  returnTaskLIMarkup(task) {
-    const listItem = document.createElement('li');
-    listItem.className = 'd-flex item';
-    listItem.id = `task-${task.index}`;
-    listItem.setAttribute('draggable', true);
-    const label = document.createElement('label');
-    label.id = `label-${task.index}`;
-    // label.setAttribute('for', `task${task.index}`);
-    const status = document.createElement('input');
-    status.setAttribute('type', 'checkbox');
-    status.id = `task${task.index}`;
-    status.setAttribute('name', `status-${task.index}`);
-    status.className = 'status-input';
-
-    listItem.appendChild(status);
-    label.append(task.description);
-    // listItem.appendChild(label);
-    const controls = document.createElement('div');
-    controls.id = `controls-${task.index}`;
-    controls.classList.add('controls');
-
-    const ellipsisBtn = document.createElement('span');
-    ellipsisBtn.classList.add('ellispis-btn');
-    ellipsisBtn.classList.add('d-flex');
-    ellipsisBtn.id = `ellispis-btn-${task.index}`;
-    ellipsisBtn.innerHTML = '<i class="fa-solid fa-ellipsis-vertical"></i>';
-
-    const trashBtn = document.createElement('span');
-    // const trashCheckbox = document.createElement('input');
-    // trashCheckbox.setAttribute('type', 'checkbox');
-    // trashCheckbox.id = `trash-checkbox-${task.index}`;
-    // trashBtn.appendChild(trashCheckbox);
-    trashBtn.classList.add('trash-btn');
-    trashBtn.classList.add('d-flex');
-    trashBtn.classList.add('d-none');
-    trashBtn.id = `trash-btn-${task.index}`;
-    trashBtn.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
-
-    controls.appendChild(label);
-    const editInput = document.createElement('input');
-    // editInput.setAttribute('type', 'text');
-    editInput.className = 'edit-input d-none';
-    editInput.value = task.description;
-    editInput.type = 'text';
-    editInput.id = `edit-input-${task.index}`;
-    controls.appendChild(editInput);
-    listItem.appendChild(controls);
-    listItem.appendChild(ellipsisBtn);
-    listItem.appendChild(trashBtn);
-
-    this.getCollection();
-
-    return listItem;
   }
 
   getTask(id) {
@@ -218,6 +154,11 @@ export default class TaskCollection {
     );
     this.collection[idx].description = data;
     populateStorage(this.getCollection());
+  }
+
+  getUncompletedTasksNo() {
+    const uncompletedTasks = this.collection.filter((task) => !task.completed);
+    return uncompletedTasks.length;
   }
 
   clearAll() {
